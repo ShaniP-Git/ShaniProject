0e00e6a8b2ca80125980b59c39191ffec300f82e
shani <shanipere123@gmail.com>
2025-08-11 13:59:43 +0300

    first commit

---
+++ jb_60/function_example/avg_calc.py
@@ -0,0 +1,11 @@
+from jb_60.function_example.utilitis import utilitis
+
+utilitis_2 = utilitis()
+
+numbers_1 = [10,20,30]
+numbers_2 = [5,10,15]
+numbers_3 = [25,10,30]
+
+total_1 = utilitis_2.avg_calc(numbers_1)
+total_2 = utilitis_2.avg_calc(numbers_2)
+total_3 = utilitis_2.avg_calc(numbers_3)
